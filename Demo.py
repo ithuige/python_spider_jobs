d89fc9b07b8411d66dd542721362490bc68b09ca
ithuige <ithuige@163.com>
2017-11-01 22:03:18 +0800

    测试

---
+++ Demo.py
@@ -0,0 +1,16 @@
+import keyword
+
+# python保留字
+print(keyword.kwlist)
+
+counter = 100  # 整型变量
+miles = 1000.0  # 浮点型变量
+name = "runoob"  # 字符串
+
+print(counter)
+print(miles)
+print(name)
+
+# int、float、bool、complex（复数）。
+a, b, c, d = 20, 5.5, True, 4 + 3j
+print(type(a), type(b), type(c), type(d))
--- main.py
+++ main.py
@@ -1,20 +1,16 @@
 #!/usr/bin/env python
-#encoding=utf-8
+# encoding=utf-8
 # python3.5
 __author__ = 'Tim Liu'
 __date__ = '2017/5/5 17:52'
 
-
 import time
-from datetime import datetime,timezone,timedelta
+from datetime import datetime, timezone, timedelta
 
 import urllib
 from urllib.request import Request, urlopen
 # import  urllib.request
-from urllib.error import URLError, HTTPError,ContentTooShortError
-
-
-
+from urllib.error import URLError, HTTPError, ContentTooShortError
 
 import bs4
 from bs4 import BeautifulSoup
@@ -22,7 +18,6 @@ from bs4 import BeautifulSoup
 import re
 import codecs
 
-import logging
 import logging.config
 
 import os
@@ -43,23 +38,26 @@ def get_east8_date_str(format_long=True):
     else:
         return dt.astimezone(timezone(timedelta(hours=8))).strftime("%Y-%m-%d")
 
-#获取51job.com 的html页面
+
+# 获取51job.com 的html页面
 def get_51job_html(jobearea, keyword):
     url_temp = "http://search.51job.com/jobsearch/search_result.php?fromJs=1&jobarea={jobarea}&keyword={keyword}&keywordtype=2&lang=c&stype=2&postchannel=0000&fromType=1&confirmdate=9"
-    url = url_temp.format(jobarea=jobearea,keyword=urllib.request.quote(keyword))
-    headers = { 'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.110 Safari/537.36' }
+    url = url_temp.format(jobarea=jobearea, keyword=urllib.request.quote(keyword))
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.110 Safari/537.36'}
 
     time.sleep(2)
 
     req = urllib.request.Request(url, None, headers)
     urlopen_content = urlopen(req)
     # urlopen_content = urllib.request.urlopen(url)  # 打开网址
-    html = urlopen_content.read()   # 读取源代码并转为gbk
+    html = urlopen_content.read()  # 读取源代码并转为gbk
     urlopen_content.close()
     if html:
         return html.decode('gbk')
     else:
         return ""
+
 
 # 解析51job.com 页面的
 def parse_51job_html_job_nums(html):
@@ -68,7 +66,7 @@ def parse_51job_html_job_nums(html):
     soup = BeautifulSoup(html, "html.parser")
     # <div class="dw_table" id="resultList">
     # //*[@id="resultList"]/div[1]/div[3]
-    #div1 = soup.find_all(id='resultList')
+    # div1 = soup.find_all(id='resultList')
 
     # class 是python的关键字，记得加下划线,
     # 第3个div： <div class="rt">共3818条职位</div>
@@ -86,23 +84,24 @@ def parse_51job_html_job_nums(html):
                 if child.string:
                     # 用正则先去掉空格这些字符
                     compile_re = re.compile('\s*')
-                    str = compile_re.sub('',child.string)
-                    #logger.info("======b===", str)
+                    str = compile_re.sub('', child.string)
+                    # logger.info("======b===", str)
                     # 用正则取出数字
                     match_re = re.match(r".*?(\d+).*", str)
                     if match_re:
                         job_nums = int(match_re.group(1))
                     break
 
-    #logger.info("51job_nums=", job_nums)
+    # logger.info("51job_nums=", job_nums)
     return job_nums
 
-#获取 智联招聘：zhaopin.com 的html页面
+
+# 获取 智联招聘：zhaopin.com 的html页面
 def get_zhaopin_html(jobarea_name, job_type):
     url_temp = "http://sou.zhaopin.com/jobs/searchresult.ashx?jl={jobarea_name}&kw={job_type}&sm=0&p=1&source=1"
-    url = url_temp.format(jobarea_name=urllib.request.quote(jobarea_name),job_type=urllib.request.quote(job_type))
-    headers = { 'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.110 Safari/537.36' }
-
+    url = url_temp.format(jobarea_name=urllib.request.quote(jobarea_name), job_type=urllib.request.quote(job_type))
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.110 Safari/537.36'}
 
     logger.warning("-------->15-->02")
     try:
@@ -113,7 +112,7 @@ def get_zhaopin_html(jobarea_name, job_type):
 
     except HTTPError as e1:
         print("The (www.python.org)server couldn't fulfill the request.")
-        logger.error('-------->15-->02->HTTPError-> %s' % ( e1.msg))
+        logger.error('-------->15-->02->HTTPError-> %s' % (e1.msg))
     except URLError as e2:
         print('We failed to reach a server.')
         logger.error('-------->15-->03->URLError->%s' % (e2.msg))
@@ -126,7 +125,7 @@ def get_zhaopin_html(jobarea_name, job_type):
         try:
             if response:
                 logger.warning("-------->15-->05-->01a")
-                html = response.read()   # 读取源代码并转为unicode
+                html = response.read()  # 读取源代码并转为unicode
                 logger.warning("-------->15-->05-->01b")
             else:
                 logger.warning("-------->15-->05-->02")
@@ -137,7 +136,6 @@ def get_zhaopin_html(jobarea_name, job_type):
             response.close()
 
         logger.warning("-------->15-->08")
-
 
         if html:
             return html.decode('UTF-8')
@@ -154,14 +152,12 @@ def parse_zhaopin_html_job_nums(html):
     soup = BeautifulSoup(html, "html.parser")
     # <span class="search_yx_tj">共<em>5631</em>个职位满足条件</span>
     # 使用css解析器
-    em = soup.select("span.search_yx_tj > em" )
-    #logger.info(u"zhipin_job_nums=", em[0].string)
+    em = soup.select("span.search_yx_tj > em")
+    # logger.info(u"zhipin_job_nums=", em[0].string)
     return int(em[0].string)
 
 
-
-
-def save_sqlite(published_time,jobarea_name, job_nums, job_type, job_site ):
+def save_sqlite(published_time, jobarea_name, job_nums, job_type, job_site):
     """
         把爬取的数据写入sqlite数据库
     :param published_time: 职位爬取的时间
@@ -172,16 +168,18 @@ def save_sqlite(published_time,jobarea_name, job_nums, job_type, job_site ):
     :return:
     """
     dbPath = '%s/jobs_analysis.db' % os.getcwd()
-    #logger.info(dbPath)
+    # logger.info(dbPath)
     con = sqlite3.connect(dbPath)
     cur = con.cursor()
     # 如果表不存在就新建表
-    cur.execute('CREATE TABLE  IF NOT EXISTS  jobs_tb (id integer  NOT NULL  PRIMARY KEY AUTOINCREMENT DEFAULT 0,published_time Varchar(20) DEFAULT "",jobarea_name Varchar(20) DEFAULT "",job_nums int DEFAULT 0, job_type Varchar(20) DEFAULT "", job_site Varchar(50) DEFAULT "", job_mark Varchar(255) DEFAULT "" )')
+    cur.execute(
+        'CREATE TABLE  IF NOT EXISTS  jobs_tb (id INTEGER  NOT NULL  PRIMARY KEY AUTOINCREMENT DEFAULT 0,published_time VARCHAR(20) DEFAULT "",jobarea_name VARCHAR(20) DEFAULT "",job_nums INT DEFAULT 0, job_type VARCHAR(20) DEFAULT "", job_site VARCHAR(50) DEFAULT "", job_mark VARCHAR(255) DEFAULT "" )')
     con.commit()
 
     # r 表示不转义，保留原始字符
-    sqlStr = r'INSERT INTO jobs_tb (id,published_time, jobarea_name, job_nums, job_type, job_site) VALUES(NULL, "%s",  "%s", "%d", "%s", "%s")' % (published_time, jobarea_name, job_nums , job_type, job_site)
-    #logger.info(sqlStr)
+    sqlStr = r'INSERT INTO jobs_tb (id,published_time, jobarea_name, job_nums, job_type, job_site) VALUES(NULL, "%s",  "%s", "%d", "%s", "%s")' % (
+    published_time, jobarea_name, job_nums, job_type, job_site)
+    # logger.info(sqlStr)
     cur.execute(sqlStr)
     con.commit()
 
@@ -200,25 +198,25 @@ def is_need_save_db(current_date, job_site):
     appconf_path = '%s/conf/app.conf' % os.getcwd()
     with codecs.open(appconf_path, 'rb', encoding='utf-8') as f:
         cp.read_file(f)
-    saved_date = cp.get('ini', 'save_date_'+ job_site)
+    saved_date = cp.get('ini', 'save_date_' + job_site)
     if current_date > saved_date:
-        cp.set('ini', 'save_date_'+ job_site, current_date)
+        cp.set('ini', 'save_date_' + job_site, current_date)
         with codecs.open(appconf_path, 'w', encoding='utf-8') as f:
             cp.write(f)
         return True
     else:
-        return  False
+        return False
 
-def spider_jobs(is_need_save=False,  job_site="51job.com", job_type = 'python', jobarea_names=[], jobarea_codes=[]):
+
+def spider_jobs(is_need_save=False, job_site="51job.com", job_type='python', jobarea_names=[], jobarea_codes=[]):
     jobs_txt_path = '%s/jobs.txt' % os.getcwd()
     file = codecs.open(jobs_txt_path, "a", "utf-8")
     file.write(u"----------------- " + job_type + " -----------------\n")
 
-
     for j in range(len(jobarea_names)):
         # 日期时间
         time_str = get_east8_date_str(True)
-        #time_str = time.strftime('%Y-%m-%d  %H:%M:%S', time.localtime(time.time()))
+        # time_str = time.strftime('%Y-%m-%d  %H:%M:%S', time.localtime(time.time()))
         # 工作地名
         jobarea_name = jobarea_names[j]
 
@@ -230,17 +228,14 @@ def spider_jobs(is_need_save=False,  job_site="51job.com", job_type = 'python', 
         elif job_site == "zhaopin.com":
             logger.debug("-------->15")
             html = get_zhaopin_html(jobarea_name, job_type)
-            logger.debug ("-------->16")
+            logger.debug("-------->16")
             # 工作的职位数
             job_nums = parse_zhaopin_html_job_nums(html)
             # 说明报异常了，不用保存及显示
             if job_nums == 0:
                 continue
-            logger.debug ("-------->17")
+            logger.debug("-------->17")
             job_nums_str = u"共" + str(job_nums) + u"条职位"
-
-
-
 
         if is_need_save:
             logger.info(u"--->Now is saving! job_nums=%d" % job_nums)
@@ -250,16 +245,13 @@ def spider_jobs(is_need_save=False,  job_site="51job.com", job_type = 'python', 
             # 已经保存过sqlite数据库了，就不保存了
             logger.info(u"--->Today was saved! job_nums=%d" % job_nums)
 
-
-
         # 数据保存到txt文本文件中
-        file.write(u"{0}\t{1}\t{2} \n".format(time_str, jobarea_name , job_nums_str ))
-
-
+        file.write(u"{0}\t{1}\t{2} \n".format(time_str, jobarea_name, job_nums_str))
 
     file.close()
 
-def search_jobs(job_sites, keywords, jobarea_names, jobarea_codes ):
+
+def search_jobs(job_sites, keywords, jobarea_names, jobarea_codes):
     for job_site in job_sites:
         jobs_txt_path = '%s/jobs.txt' % os.getcwd()
         file = codecs.open(jobs_txt_path, "a", "utf-8")
@@ -267,13 +259,13 @@ def search_jobs(job_sites, keywords, jobarea_names, jobarea_codes ):
         file.close()
 
         # 判断sqlite数据库是否要保存今天的记录
-        current_date =  get_east8_date_str(False)
-        #current_date = time.strftime('%Y-%m-%d', time.localtime(time.time()))
+        current_date = get_east8_date_str(False)
+        # current_date = time.strftime('%Y-%m-%d', time.localtime(time.time()))
 
         is_need_save = is_need_save_db(current_date, job_site)
 
         for i in range(len(keywords)):
-            spider_jobs(is_need_save, job_site, keywords[i], jobarea_names,jobarea_codes )
+            spider_jobs(is_need_save, job_site, keywords[i], jobarea_names, jobarea_codes)
 
 
 if __name__ == '__main__':
@@ -284,19 +276,12 @@ if __name__ == '__main__':
     job_sites = ["51job.com", "zhaopin.com"]
 
     # 机器学习、数据挖掘 、深度学习、架构师
-    keywords = [u"人工智能", u"大数据","java",u"前端", "Android", "iOS", "python", "php", "golang"]
-
+    keywords = [u"人工智能", u"大数据", "java", u"前端", "Android", "iOS", "python", "php", "golang"]
 
     # 51job用：北京 ： jobarea=010000，
-    jobarea_names = [u"北京",    u"上海",  u"深圳",   u"广州",   u"杭州"]
-    jobarea_codes  = ["010000", "020000", "040000", "030200", "080200"]
+    jobarea_names = [u"北京", u"上海", u"深圳", u"广州", u"杭州"]
+    jobarea_codes = ["010000", "020000", "040000", "030200", "080200"]
 
     logger.info(u"============>bengin...")
-    search_jobs(job_sites, keywords, jobarea_names, jobarea_codes );
+    search_jobs(job_sites, keywords, jobarea_names, jobarea_codes)
     logger.info(u"============>end!")
-
-
-
-
-
-
